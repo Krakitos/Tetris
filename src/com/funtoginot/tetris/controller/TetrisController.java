ac441aca031556c6b6f8fcf09aadd20944f3c9bc
Krakitos <funtowiczmo@gmail.com>
2014-05-15 10:08:24 +0200

    Gestion des évènements clavier

--- src/com/funtoginot/tetris/controller/TetrisController.java
+++ src/com/funtoginot/tetris/controller/TetrisController.java
@@ -4,26 +4,52 @@ import com.funtoginot.tetris.data.TetrisEngine;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseListener;
 
 /**
  * Created by Morgan on 14/05/2014.
  */
-public class TetrisController implements KeyListener{
+public class TetrisController implements KeyListener {
+
     private TetrisEngine engine;
+
+    /**
+     * Méthode de traitement pour la mise en pause du jeu
+     */
+    public void handlePauseAction(){
+        engine.togglePause();
+    }
+
+    /**
+     * Méthode de traitement pour démarrer le jeu
+     */
+    public void handleStartAction(){
+        if(!engine.isPlaying()){
+            engine.startGame();
+        }
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
-
+        if(isValidKeyboardInput(e.getKeyCode())) {
+            engine.handleKeyPressed(e.getKeyCode());
+        }
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
+    public void keyPressed(KeyEvent e) {}
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(KeyEvent e) {}
 
+    /**
+     * Verifie que la touche pressée par l'utilisateur est valide
+     * @param keycode Le code de la touche pressée
+     * @return True si la touche pressée est valide (mouvement autorisé), false sinon
+     */
+    private boolean isValidKeyboardInput(int keycode){
+        return keycode == KeyEvent.VK_SPACE
+                || keycode == KeyEvent.VK_DOWN
+                || keycode == KeyEvent.VK_RIGHT
+                || keycode == KeyEvent.VK_LEFT;
     }
 }
