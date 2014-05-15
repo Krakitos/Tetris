455db22d02243f34dfac12478662ec3d7474d888
Krakitos <funtowiczmo@gmail.com>
2014-05-15 08:27:25 +0200

    Ajout des tetrominos

--- src/com/funtoginot/tetris/data/tetrominos/TetrominosFactory.java
+++ src/com/funtoginot/tetris/data/tetrominos/TetrominosFactory.java
@@ -1,6 +1,7 @@
 package com.funtoginot.tetris.data.tetrominos;
 
 import java.awt.*;
+import java.util.Arrays;
 
 /**
  * Created by Morgan on 14/05/2014.
@@ -11,25 +12,32 @@ public class TetrominosFactory {
     private static final Color[] tetrominos_colors = new Color[7];
 
     static{
+        //I
         tetrominos[0] = new byte[][]{{1,1,1,1},{0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
         tetrominos_colors[0] = Color.RED;
 
-        tetrominos[1] = null;
+        //O
+        tetrominos[1] = new byte[][]{{1,1},{1,1}};
         tetrominos_colors[1] = Color.BLUE;
 
-        tetrominos[2] = null;
+        //T
+        tetrominos[2] = new byte[][]{{1, 1, 1}, {0, 1, 0}, {0, 0, 0}};
         tetrominos_colors[2] = Color.GRAY;
 
-        tetrominos[3] = null;
+        //L
+        tetrominos[3] = new byte[][]{{1, 0, 0}, {1, 0, 0}, {1, 1, 0}};
         tetrominos_colors[3] = Color.MAGENTA;
 
-        tetrominos[4] = null;
+        //J
+        tetrominos[4] = new byte[][]{{0, 0, 1}, {0, 0, 1}, {0, 1, 1}};
         tetrominos_colors[4] = Color.WHITE;
 
-        tetrominos[5] = null;
+        //S
+        tetrominos[5] = new byte[][]{{0, 1, 1}, {1, 1, 0}, {0, 0, 0}};
         tetrominos_colors[5] = Color.GREEN;
 
-        tetrominos[6] = null;
+        //Z
+        tetrominos[6] = new byte[][]{{1, 1, 0}, {0, 1, 1}, {0, 0, 0}};
         tetrominos_colors[6] = Color.CYAN;
     }
 
@@ -42,10 +50,20 @@ public class TetrominosFactory {
     public Tetromino getTetromino(){
         int index = (int)(Math.random() * (tetrominos.length - 1));
 
-        return new Tetromino(tetrominos_colors[index], new byte[][]{{1,1,1,1},{0,0,0,0}, {0,0,0,0}, {0,0,0,0}});
+        return new Tetromino(tetrominos_colors[index], cloneTetromino(index));
     }
 
     public static TetrominosFactory getInstance() {
         return ourInstance;
+    }
+
+    private byte[][] cloneTetromino(int index) {
+        byte[][] result = new byte[tetrominos[index].length][];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Arrays.copyOf(tetrominos[index][i], tetrominos[index][i].length);
+        }
+
+        return result;
     }
 }
