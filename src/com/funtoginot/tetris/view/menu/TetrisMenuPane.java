687e66777916565cee264fdf363159eba66153cc
Morgan Funtowicz <funtowiczmo@gmail.com>
2014-05-23 17:41:14 +0200

    Affichage prévisualisation tetromino 2

--- src/com/funtoginot/tetris/view/menu/TetrisMenuPane.java
+++ src/com/funtoginot/tetris/view/menu/TetrisMenuPane.java
@@ -1,5 +1,8 @@
 package com.funtoginot.tetris.view.menu;
 
+import com.funtoginot.tetris.data.tetrominos.Tetromino;
+import com.funtoginot.tetris.view.components.TetrominoPreview;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,12 +14,9 @@ public class TetrisMenuPane extends JPanel {
     private JPanel partieCours;
     private JPanel boutons;
 
+    private TetrominoPreview preview;
 
     public TetrisMenuPane() {
-
-
-        //Ajout du premier Label
-        JLabel label = new JLabel("Tetris Game");
 
         //Ajout d'un 1er bouton
         JButton boutonStart = new JButton("START");
@@ -29,19 +29,23 @@ public class TetrisMenuPane extends JPanel {
 
         //On crée et ajoute les deux Panels qui vont êtres insérés dans les deux lignes du GridLayout
         partieCours = new JPanel();
+        preview = new TetrominoPreview();
         boutons = new JPanel();
-        add(partieCours);
-        add(boutons);
 
         //A l'intérieur de ces derniers, on insère deux GridLayout
-        partieCours.setLayout(new GridLayout(2, 1));
+        partieCours.setLayout(new GridLayout(1, 1));
         boutons.setLayout(new GridLayout(2, 1));
 
         //On ajoute les éléments souhaités
-        partieCours.add(label);
+        partieCours.add(preview);
         boutons.add(boutonStart);
         boutons.add(boutonQuit);
 
+        add(partieCours);
+        add(boutons);
+    }
 
+    public void updateNextTetromino(Tetromino tetromino){
+        preview.updateView(tetromino);
     }
 }
